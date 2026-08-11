039adabe330b8fc8dd92bf9f6ab83b490400a44e
Laurent Hoxha <130primex.eu@gmail.com>
2026-08-11 13:31:40 +0200

    deploy fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,10 @@ const deploymentId =
   process.env.NEXT_DEPLOYMENT_ID ||
   process.env.GITHUB_SHA ||
   process.env.APP_BUILD_SHA;
+const distDir = process.env.NEXT_DIST_DIR || ".next";
 
 const nextConfig: NextConfig = {
+  distDir,
   ...(deploymentId ? { deploymentId } : {}),
   typescript: {
     ignoreBuildErrors: true,
